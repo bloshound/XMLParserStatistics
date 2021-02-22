89fceceacb889d8cad4487a3d3b12c76d9aaa4ac
bloshound <bloshound@gmail.com>
2021-02-22 23:04:26 +0300

    work1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,8 @@ import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -20,20 +19,24 @@ public class Main {
 
     static StatsUtil util = StatsUtil.INSTANCE;
 
-
     public static void main(String[] args) {
+        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 
-        try (BufferedInputStream buffIS = new BufferedInputStream(new FileInputStream("D:\\address.xml"))) {
+        while (true) {
 
-            XMLEventReader reader = util.createReader(buffIS);
-            util.findCoincidences(reader, "item", 2);
+            System.out.print("Введите путь к файлу: ");
+            try (BufferedInputStream fileStream = new BufferedInputStream(new FileInputStream(consoleReader.readLine()))) {
 
-            findHouseFloors();
 
-        } catch (IOException | XMLStreamException e) {
-            e.printStackTrace();
+            } catch (FileNotFoundException e) {
+                System.out.println("Не удается найти указзанный файл.");
+
+            } catch (IOException e) {
+                System.out.println("Ошибка чтения.");
+
+
+            }
         }
-
     }
 
 
@@ -43,10 +46,9 @@ public class Main {
         QName floorQName = new QName("floor");
 
         BiFunction<int[], int[], int[]> mergeIntArrays = (ints1, ints2) -> {
-            if (ints1.length != ints2.length) throw new IllegalArgumentException("массивы разного размера");
+            if (ints1.length != ints2.length) throw new IllegalArgumentException("Массивы разного размера");
 
             int[] result = new int[ints1.length];
-
             for (int i = 0; i < ints1.length; i++) {
                 result[i] = ints1[i] + ints2[i];
             }
@@ -68,28 +70,21 @@ public class Main {
                 if (event.isStartElement()
                         && event.asStartElement().getAttributeByName(cityQName) != null
                         && event.asStartElement().getAttributeByName(floorQName) != null) {
-re
+
                     StartElement startElement = event.asStartElement();
                     String city = startElement.getAttributeByName(cityQName).getValue();
                     int floor = Integer.parseInt(startElement.getAttributeByName(floorQName).getValue());
 
                     floors[floor] = floors[floor] + 1;
-                 //   System.out.println(Arrays.toString(floors));
-
-
                     floorHousesByCity.merge(city, floors, mergeIntArrays);
                 }
             }
 
             floorHousesByCity.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(pair -> System.out.println(
-                    "город " + pair.getKey() + ": " +  Arrays.toString(pair.getValue())));
+                    "город " + pair.getKey() + ": " + Arrays.toString(pair.getValue())));
 
         } catch (IOException | XMLStreamException e) {
             e.printStackTrace();
         }
-
-
     }
-
-
 }
