a2b6820b845ba4d61de8c5771b3e6c0378f54d43
bloshound <bloshound@gmail.com>
2021-02-22 18:01:40 +0300

    work1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * created by bloshound
@@ -18,7 +19,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try(BufferedInputStream buffIS = new BufferedInputStream(new FileInputStream("D:\\address.xml"))) {
+        try (BufferedInputStream buffIS = new BufferedInputStream(new FileInputStream("D:\\address.xml"))) {
 
             XMLEventReader reader = util.createReader(buffIS);
             util.findCoincidences(reader, "item", 2);
@@ -30,23 +31,27 @@ public class Main {
     }
 
 
-    private static void findHouseFloors(XMLEventReader reader, S){
-        StringBuilder sb = new StringBuilder();
+    private static void findHouseFloors() {
+
         int[] floors = {1, 1, 1, 1, 1};
 
-        try(BufferedInputStream buffIS = new BufferedInputStream(new FileInputStream("D:\\address.xml"))) {
+        QName cityQName = new QName("city");
+        QName floorQName = new QName("floor");
+
+        HashMap<String, int[]> floorHousesByCity = new HashMap<>();
+
+        try (BufferedInputStream buffIS = new BufferedInputStream(new FileInputStream("D:\\address.xml"))) {
             XMLEventReader reader = util.createReader(buffIS);
 
-            while (reader.hasNext()){
-
-                StartElement element = util.getStartElementByName(reader, "item"){
+            while (reader.hasNext()) {
+                XMLEvent event = reader.nextEvent();
+                if (event.isStartElement()
+                        && event.asStartElement().getAttributeByName(cityQName) != null
+                        && event.asStartElement().getAttributeByName(floorQName) != null) {
 
                 }
 
             }
-
-
-
 
 
         } catch (IOException | XMLStreamException e) {
@@ -54,9 +59,7 @@ public class Main {
         }
 
 
-
     }
-
 
 
 }
--- src/main/java/StatsUtil.java
+++ src/main/java/StatsUtil.java
@@ -1,7 +1,6 @@
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.events.Attribute;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 import java.io.InputStream;
@@ -21,26 +20,9 @@ public class StatsUtil {
 
     //получение XML ридера
     public XMLEventReader createReader(InputStream is) throws XMLStreamException {
-        return new StaxEventProcessor(is).getReader();
-    }
+        return new StaxEventProcessor(is).getReader();    }
 
-    public StartElement getStartElementByName(XMLEventReader reader, String name) throws XMLStreamException {
-        QName qName = new QName(name);
-        StartElement startElement = null;
 
-        XMLEvent event = reader.nextEvent();
-        if (event.isStartElement() && event.asStartElement().getName().equals(qName)) {
-            startElement = event.asStartElement();
-        }
-
-        return startElement;
-
-    }
-
-    public Attribute getAttributeByName(StartElement element, String name) {
-        QName qName = new QName(name);
-        return element.getAttributeByName(qName);
-    }
 
     //2 метода - получение уникальных элементов c колличеством повторений. ПОлучаем String так как определен HashCode
     public HashMap<String, Integer> getUniqueStartElements(InputStream is, String name) throws XMLStreamException {
@@ -48,14 +30,16 @@ public class StatsUtil {
         return getUniqueStartElements(reader, name);
     }
 
-
     public HashMap<String, Integer> getUniqueStartElements(XMLEventReader reader, String name) throws XMLStreamException {
-
+        QName qName = new QName(name);
         HashMap<String, Integer> startElements = new HashMap<>();
+
         while (reader.hasNext()) {
-            StartElement startElement = getStartElementByName(reader, name);
-            if (startElement != null) {
-                startElements.merge(startElement.toString(), 1, Integer::sum);
+            XMLEvent event = reader.nextEvent();
+
+            if (event.isStartElement() && event.asStartElement().getName().equals(qName)) {
+                String startElement = event.toString();
+                startElements.merge(startElement, 1, Integer::sum);
             }
         }
         return startElements;
